59b20b0ed4871cc84df9edb7f290accdef08652d
mastachii <mastachii273@gmail.com>
2024-12-26 12:19:46 +0800

    feat: Integrated express-session

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const PgStore = require("connect-pg-simple")(session);
+const pool = require("./db/pool");
+require("dotenv").config();
 
 // Routes
 const signUp = require("./routes/signUpRoutes");
 
 const app = express();
+
+// Session stuff
+const sessionStore = new PgStore({
+    pool,
+    tableName: "sessions",
+    createTableIfMissing: true,
+});
+
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: sessionStore,
+        cookie: {
+            maxAge: 120000, // Set cookies to 2 minutes for debugging purposes, will set to one day once ready for prod.
+        },
+    })
+);
+
 
 app.set("view engine", "ejs");
 
--- db/pool.js
+++ db/pool.js
@@ -1,6 +1,5 @@
-require("dotenv").config();
-
 const { Pool } = require("pg");
+require("dotenv").config();
 
 module.exports = new Pool({
     connectionString: process.env.DATABASE_LOCAL,
