99da3b06871b8582748bc4cdf08ec4f26118d742
mastachii <mastachii273@gmail.com>
2024-12-29 00:48:27 +0800

    feat: Form validation for creating new messages

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -6,26 +6,17 @@ const titleMessage = "The title may not be longer than 255 characters.";
 const bodyMessage = "The message may not be longer than 10,000 characters."; // Error message for the message itself.
 
 const validateForm = [
-    body("title").notEmpty().withMessage("Title is required.").isLength({ max: 255 }).withMessage(titleMessage),
-    body("message").notEmpty().withMessage("Message is required.").isLength({ max: 10000 }).withMessage(bodyMessage),
+    body("title").trim().notEmpty().withMessage("Title is required.").isLength({ max: 255 }).withMessage(titleMessage),
+    body("message").trim().notEmpty().withMessage("Message is required.").isLength({ max: 10000 }).withMessage(bodyMessage),
 ];
 
-// const addMessage = [
-//     validateForm,
-//     (req, res, next) => {
-//         const errors = validationResult(req);
-//         if (!errors.isEmpty())
-//     },
-// ];
-
-async function addMessage(req, res) {
-    const { title, message } = req.body;
-    const username = req.user.username;
-    const date = new Date().toUTCString();
-
-    await db.addMessage({ title, message, username, date });
-    res.redirect("/");
-}
+const addMessage = [
+    validateForm,
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) res.json({ errors: errors.array() });
+    },
+];
 
 module.exports = {
     addMessage,
