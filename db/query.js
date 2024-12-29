018c385eaf4c8db570673fcdaf0c7102cd378823
mastachii <mastachii273@gmail.com>
2024-12-29 23:44:32 +0800

    refactor: Switched database to hosted instead of local

--- db/query.js
+++ db/query.js
@@ -1,5 +1,32 @@
 const db = require("./pool");
 
+function createTableUsers() {
+    db.query(`
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            fullname VARCHAR ( 255 ),
+            username VARCHAR ( 255 ),
+            email VARCHAR ( 255 ),
+            password VARCHAR ( 255 ),
+            pfp_url VARCHAR ( 255 ),
+            is_member BOOLEAN,
+            is_admin BOOLEAN
+        )
+    `);
+}
+
+function createTableMessages() {
+    db.query(`
+        CREATE TABLE messages (
+            id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            date VARCHAR ( 255 ),
+            username VARCHAR ( 255 ),
+            title VARCHAR ( 255 ),
+            message TEXT
+        )
+    `);
+}
+
 async function addUser({ fullname, email, username, password, isAdmin, pfpUrl }) {
     await db.query("INSERT INTO users (fullname, email, username, password, is_admin, pfp_url,is_member) VALUES ($1, $2, $3, $4, $5, $6, FALSE)", [
         fullname,
